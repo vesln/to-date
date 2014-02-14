0c31fc5f3919f17f0b739f9b4b0a0a4c9f500a1d
Veselin Todorov <veselin@veselin.bg>
2014-02-14 19:12:53 +0200

    Convert to a class

--- index.js
+++ index.js
@@ -5,44 +5,133 @@
 var ago = require('ago');
 
 /**
+ * Supported units.
+ */
+
+var units = [
+  'second',
+  'seconds',
+  'minute',
+  'minutes',
+  'hour',
+  'hours',
+  'day',
+  'days',
+  'week',
+  'weeks',
+  'month',
+  'months',
+  'year',
+  'years',
+];
+
+/**
+ * To Date.
+ *
+ * @param {Number} amount
+ * @param {String} unit [optional]
+ * @param {String} method [optional]
+ * @constructor
+ */
+
+function ToDate(amount, unit, method) {
+  this.amount = amount;
+  this.method = method;
+  this.unit = unit;
+}
+
+/**
  * Parse `str` and return a new date.
  *
  * Examples:
  *
  * ```js
- * toDate('1 day from now');
- * toDate('23 days from now');
- * toDate('23 minutes from now');
+ * ToDate.parse('1 day from now');
+ * ToDate.parse('23 days from now');
+ * ToDate.parse('23 minutes from now');
  *
- * toDate('1 day ago');
- * toDate('11 months ago');
+ * ToDate.parse('1 day ago');
+ * ToDate.parse('11 months ago');
  * ```
  *
- * @param {String} str
+ * @param {String} input
  * @returns {Date}
  * @api public
  */
 
-function toDate(str) {
-  var parts = str.split(' ');
-  var amount = parts.shift();
-  var unit = parts.shift();
-  var method = parts.join(' ');
+ToDate.parse = function(input) {
+  var parts = input.split(' ');
+  return new ToDate(parts.shift(), parts.shift(), parts.join(' ')).build();
+};
+
+/**
+ * Set the method to "from now" and return the desired date.
+ *
+ * @returns {Date}
+ * @api public
+ */
+
+ToDate.prototype.fromNow = function() {
+  this.method = 'from now';
+  return this.build();
+};
+
+/**
+ * Set the method to "ago" and return the desired date.
+ *
+ * @returns {Date}
+ * @api public
+ */
+
+ToDate.prototype.ago = function() {
+  this.method = 'ago';
+  return this.build();
+};
+
+/**
+ * Build the desired date and return it.
+ *
+ * @returns {Date}
+ * @api public
+ */
+
+ToDate.prototype.build = function() {
   var time = null;
 
-  if (method === 'from now') {
-    time = ago.fromNow(amount, unit);
-  } else if (method === 'ago') {
-    time = ago(amount, unit);
+  if (this.method === 'from now') {
+    time = ago.fromNow(this.amount, this.unit);
+  } else if (this.method === 'ago') {
+    time = ago(this.amount, this.unit);
   } else {
-    throw new Error('Invalid method: ' + method);
+    throw new Error('Invalid method: ' + this.method);
   }
 
   return new Date(time);
-}
+};
 
 /**
- * Primary export.
+ * Set all units as getters.
  */
 
-module.exports = toDate;
+units.forEach(function(unit) {
+  Object.defineProperty(ToDate.prototype, unit, {
+    get: function() {
+      this.unit = unit;
+      return this;
+    }
+  });
+});
+
+/**
+ * Handle input.
+ *
+ * @param {String|Number} str
+ * @returns {Date|ToDate}
+ * @api public
+ */
+
+module.exports = function(input) {
+  return typeof input === 'number'
+    ? new ToDate(input)
+    : ToDate.parse(input);
+};
